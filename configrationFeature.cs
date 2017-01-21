b5a6f572c24f35ff8e7942b2eaa210d8076c2707
Peter Atwater <peteratwater@gmail.com>
2017-01-20 18:33:52 -0800

    configurationFeatures updated to allow for HybridMode flag

--- configrationFeature.cs
+++ configrationFeature.cs
@@ -5,5 +5,9 @@ public class configrationFeature
 {
     [XmlAttribute]
     public bool pvp;
+
+    //Add custom hybridMode flag for reading from config file. NOT the intended purpose of the features config but its w/e
+    [XmlAttribute]
+    public bool hybridMode;
 }
 
