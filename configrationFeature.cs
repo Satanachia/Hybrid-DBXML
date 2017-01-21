14564c6b904c051bd2f2e34a9f9b722ff3495d75
Peter Atwater <peteratwater@gmail.com>
2017-01-20 18:39:16 -0800

    Add characterFileAdapterFix
    Like the hybridMode flag this is was added to allow for easy enabling
    and disabling of the character table's reference to the accountref when
    runnining in incomplete hybrid mode.

--- configrationFeature.cs
+++ configrationFeature.cs
@@ -9,5 +9,8 @@ public class configrationFeature
     //Add custom hybridMode flag for reading from config file. NOT the intended purpose of the features config but its w/e
     [XmlAttribute]
     public bool hybridMode;
+    // Similar idea here. Ability to toggle the character adapter workaround when running acocuntref in SQL mode but not character. 
+    [XmlAttribute]
+    public bool characterFileAdapterFix;
 }
 
